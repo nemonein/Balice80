0334181c991d0bf7e2218a281141d4f647f9d40d
박정호 <nemonein@gmail.com>
2026-04-13 21:21:10 +0900

    NPD TG키 위치 변경 등

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -131,12 +131,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,      KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,
         KC_GRV,      KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        KC_BSPC,
-        KC_TAB,      KC_QUOT,     KC_COMM,     KC_DOT,      KC_P,        KC_Y,        TG(_NPD),
+        KC_TAB,      KC_QUOT,     KC_COMM,     KC_DOT,      KC_P,        KC_Y,        KC_NO,
         KC_CAPS,     KC_A,        KC_O,        KC_E,        KC_U,        KC_I,
         KC_LSFT,     KC_SCLN,     KC_Q,        KC_J,        KC_K,        KC_X,
         LCDQ,        LSDQ,        KC_NO,       LADQ,                     KC_SPC,
         // right hand
-        KC_F7,       KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,      KC_NO,       KC_DEL,      KC_MUTE,
+        KC_F7,       KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,      TG(_NPD),       KC_DEL,      KC_MUTE,
         KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_LBRC,     KC_RBRC,     KC_BSPC,     HM_CH,
         KC_F,        KC_G,        KC_C,        KC_R,        KC_L,        KC_SLSH,     KC_EQL,      KC_BSLS,     TD(TD_PU_PD),
         KC_D,        KC_H,        KC_T,        KC_N,        KC_S,        KC_MINS,                  KC_ENT,      ED_CE,
@@ -200,7 +200,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
         KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
         KC_NUM,      KC_PSLS,     KC_PAST,     KC_NO,       KC_NO,       KC_TRNS,     KC_NO,       KC_TRNS,     TD(TD_HM_ED),
-        KC_TRNS,     KC_P7,       KC_P8,       KC_P9,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_PGUP,
+        KC_CALC,     KC_P7,       KC_P8,       KC_P9,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_PGUP,
         KC_TRNS,     KC_P4,       KC_P5,       KC_P6,       KC_PPLS,     KC_PMNS,                  KC_TRNS,     KC_PGDN,
         KC_TRNS,     KC_P0,       KC_P1,       KC_P2,       KC_P3,       KC_PDOT,     KC_TRNS,     KC_TRNS,
                      KC_P0,                    KC_NO,       KC_TRNS,     KC_NO,       KC_TRNS,     KC_TRNS,     KC_TRNS
